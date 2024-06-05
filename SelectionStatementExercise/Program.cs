d85fe7db97839e5bf7158167569f1ea43cc9b5b5
Douglas <douglas-brumley@hotmail.com>
2024-06-05 19:21:20 -0400

    First Excercise Completed

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Excercise1(); ;
+
+
         }
+
+        public static void Excercise1()
+        {
+
+
+            int favNumber = 260;
+            Console.WriteLine("Please guess my favorite number between 1 and 1000: ");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess > favNumber)
+
+            {
+                Console.WriteLine("To Low");
+            }
+
+            else if (userGuess < favNumber)
+
+            {
+                Console.WriteLine("To High");
+            }
+
+            else
+
+            {
+                Console.WriteLine("Just Right!");
+            }
+
+    
+        }
+
+    
+
+
+
+
     }
+
+
 }
+
+ 
