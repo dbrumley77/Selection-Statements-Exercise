897fcd5b10f9b987989e1b0784541065c6830cf1
Douglas <douglas-brumley@hotmail.com>
2024-06-06 16:33:43 -0400

    Second Attempt

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Excercise1(); ;
+            Excercise1(); 
 
 
         }
@@ -17,13 +17,13 @@
             Console.WriteLine("Please guess my favorite number between 1 and 1000: ");
             int userGuess = int.Parse(Console.ReadLine());
 
-            if (userGuess > favNumber)
+            if (userGuess < favNumber)
 
             {
                 Console.WriteLine("To Low");
             }
 
-            else if (userGuess < favNumber)
+            else if (userGuess > favNumber)
 
             {
                 Console.WriteLine("To High");
